d47767fb35e63efc263eb93c8233a14c230c54b4
Owen4533 <on24131@students.sacredheart.school.nz>
2025-07-15 03:15:08 +0000

    15/07/25 - update two

--- website.py
+++ website.py
@@ -1,1 +1,3 @@
-from bottle import route, run, template, view, static_file, request, redirect, error
+from bottle import route, run, template, view, static_file, request, redirect, error 
+import requests
+from datetime import datetime
