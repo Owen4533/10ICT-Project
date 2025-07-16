61a9ec8184267d4d168851e8a570bf3d6e72f587
Owen4533 <on24131@students.sacredheart.school.nz>
2025-07-16 23:45:34 +0000

    New Finished agify

--- website.py
+++ website.py
@@ -1,23 +1,11 @@
-from bottle import route, run, template, request
+from bottle import route, run, template, view, static_file, request, redirect, error
 import requests
 from datetime import datetime
-
-
-@route('/<name>')
-def index(name):
-    response = requests.get(f"https://api.agify.io/?name={name}")
-    response = response.json()
-    age = response['age']
-    return template('index', name=name, age=age)
-
-
 @route('/')
-def default():
+def index():
     name = 'Owen'
     response = requests.get(f"https://api.agify.io/?name={name}")
     response = response.json()
     age = response['age']
     return template('index', name=name, age=age)
-
-
 run(host='0.0.0.0', port=4000, reloader=True, debug=True)
